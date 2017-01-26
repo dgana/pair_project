82aa110a5b045c12955660b94e4fbf12ec626202
Ida Bagus Chahya Dhegana <dhegana@gmail.com>
2017-01-26 20:10:50 +0700

    WIP 1

--- routes/index.js
+++ routes/index.js
@@ -4,47 +4,59 @@ let db = require('../models')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Final Project Phase 1' });
+  res.render('index');
 });
 
-// router.get('/main.ejs', function(req, res, next) {
-//   res.render('main', { title: 'Final Project Phase 1' });
-// });
+router.get('/main', function(req, res, next) {
+  res.render('main');
+});
 
 router.post('/className', function(req, res, next) {
-  db.Players.create().then(function (find) {
-    if (req.query.job === "Swordman") {
-      find.update({
-        job: string,
-        strength: 10,
-        constitution: 10,
-        intelligence: 5,
-        spirit: 5,
-        dexterity: 5
-      })
-    }
-    else if (req.query.job === "Wizard") {
-      find.update({
-        job: string,
-        strength: 5,
-        constitution: 5,
-        intelligence: 10,
-        spirit: 10,
-        dexterity: 5
-      })
-    }
-    else if (req.query.job === "Archer") {
-      find.update({
-        job: string,
-        strength: 10,
-        constitution: 5,
-        intelligence: 5,
-        spirit: 5,
-        dexterity: 10
-      })
-    }
-  });
-  res.render('main', { title: 'Final Project Phase 1' , name: req.query.job});
+  if (req.body.job === "Swordman") {
+    db.Players.create({
+      name: req.body.job,
+      job: string,
+      strength: 10,
+      constitution: 10,
+      intelligence: 5,
+      spirit: 5,
+      dexterity: 5
+    })
+  } else if (req.body.job === "Wizard") {
+    db.Players.create({
+      name: req.body.job,
+      job: string,
+      strength: 10,
+      constitution: 10,
+      intelligence: 5,
+      spirit: 5,
+      dexterity: 5
+    })
+  } else if (req.body.job === "Archer") {
+    db.Players.create({
+      name: req.body.job,
+      job: string,
+      strength: 10,
+      constitution: 5,
+      intelligence: 5,
+      spirit: 5,
+      dexterity: 10
+    }).then(function () {
+      res.redirect('/main');
+    })
+  }
 });
+
+// router.get('/todos', function(req, res, next) {
+//   models.Todo.findAll({raw: true}).then(function(todos){
+//     res.json(todos);
+//   })
+// });
+//
+// router.post('/todos/add', function(req, res, next) {
+//   models.Todo.create({title: req.body.title, completed: false}).then(function(todo){
+//     res.json(todo);
+//   })
+// });
 
 module.exports = router;
